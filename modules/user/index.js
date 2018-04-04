c0893e99ab03e8e9faa160c9dfa823cd5b2a2aab
Bhaumik Pandya <bhaumik.pandya@bluetrade.de>
2018-04-04 14:33:03 +0200

    password policy

--- modules/user/index.js
+++ modules/user/index.js
@@ -1,5 +1,6 @@
 const authentication_methods = require('../authentication');
 const required_params = ['username', 'password']; // 'role' could be added here
+const min_password_length = 6;
 
 module.exports = class User {
 	constructor() {
@@ -10,6 +11,8 @@ module.exports = class User {
 				throw new Error('Missing required parameter: ' + required_param);
 			}
 		}
+		this.validateUser();
+		this.validatePassword();
 		this.password = authentication_methods.encryptPassword(this.password);
 		this.role = arguments[0]['role'];
 		this.username = this.username.toLowerCase();
@@ -17,7 +20,6 @@ module.exports = class User {
 		this.createdUnix = (new Date()).getTime();
 		this.active = true;
 		this.role = (this.role === 'root') ? this.role : 'user';
-		this.validateUser();
 	}
 
 	validateUser() {
@@ -30,4 +32,29 @@ module.exports = class User {
 			throw e;
 		}
 	}
+
+	validatePassword() {
+		// basic password policy:
+		let regexes = [
+			/.*[A-Z]+.*/g,
+			/.*[a-z]+.*/g,
+			/.*[0-9]+.*/g,
+			/.*[!@#$%^&*]+.*$/
+		];
+		let total = 0;
+		for (let regex of regexes) {
+			if ((regex).test(this.password)) {
+				total++;
+			}
+		}
+
+		if (total !== regexes.length && this.password.length >= min_password_length) {
+			throw new Error(`Password not safe enough. \nPassword requires at least: \none capital letter \none small letter \none digit \none special character \nminimum length of ${min_password_length}. \n`);
+		}
+	}
 }
+
+new module.exports({
+	username: 'abc@domain.de',
+	password: 'abcDEF#123'
+});
