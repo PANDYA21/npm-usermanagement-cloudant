1634513c354db88b5f76668195bfa2e14889e781
Bhaumik Pandya <bar@example.com>
2018-04-09 17:23:06 +0200

    test modifications

--- test.js
+++ test.js
@@ -6,6 +6,16 @@ process.env.VCAP_SERVICES = JSON.stringify({
 
 const Usermanagement = require('.');
 let usermanagement = new Usermanagement();
-usermanagement.syncRootUser()
+
+async function testIt() {
+	try {
+		await usermanagement.syncRootUser();
+	} catch(err) {
+		console.error(err);
+	}
+	return usermanagement.getUser('root@root.root');
+}
+
+testIt()
 	.catch(err => err ? console.error(err) : null)
 	.then(d => console.log(JSON.stringify(d, null, 2)));
